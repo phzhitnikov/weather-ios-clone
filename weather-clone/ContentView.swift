fcd570056af0d8a7e3c44a5bb6550442bee7ecc8
Phil Zhitnikov <phzhitnikov@gmail.com>
2021-03-08 13:25:18 +0300

    Initial Commit

---
+++ weather-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather-clone
+//
+//  Created by Филипп Житников on 08.03.2021.
+//  Copyright © 2021 phzhitnikov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
